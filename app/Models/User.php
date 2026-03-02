9cbd511823e3cae81dd5a3702b91e48138cdf8b8
Shishir Adhikari <shishiradhikari1829@gmail.com>
2026-03-02 18:03:02 +0000

    fix: handle null user in isFollowedBy method to prevent errors

--- app/Models/User.php
+++ app/Models/User.php
@@ -74,7 +74,10 @@ class User extends Authenticatable implements MustVerifyEmail
         }
     }
 
-    public function isFollowedBy(User $user) {
+    public function isFollowedBy(?User $user) {
+        if (!$user) {
+            return false;
+        }
         return $this->followers()->where('follower_id', $user->id)->exists();
     }
 
